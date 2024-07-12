0e5fd704449192633bf68c1e2e8f534a50057275
shivang <sh587705@dal.ca>
2024-07-12 12:32:30 -0300

    Test

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const PORT = Number(process.env.PORT) || 2000;
 const FILE_DIRECTORY = process.env.FILE_DIRECTORY || "../";
 
 console.log("test")
+console.log("test")
 
 app.post("/parser", async (req, res) => {
   const { file, product } = req.body;
